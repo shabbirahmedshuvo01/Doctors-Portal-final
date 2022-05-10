1c2207d6c93dfbd1a640f7f186dce6da6a702c9e
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-11 01:10:42 +0600

    home added

---
+++ src/Pages/Home/InfoCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const InfoCard = ({ img, cardTitle, bgclass }) => {
+    return (
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgclass}`}>
+            <figure className='pl-5'>
+                <img src={img} alt="Album" />
+            </figure>
+            <div className="card-body text-white">
+                <h2 className="card-title">{cardTitle}</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
