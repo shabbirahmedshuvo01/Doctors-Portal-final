8f399c29a520d8c185825d9c46a1d3df28783bae
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-11 21:41:01 +0600

    home page ready

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,7 +7,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours" bgclass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
             <InfoCard cardTitle="Visit our location" bgclass="bg-accent" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact us now" bgclass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
---
+++ src/Pages/Home/Ready.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import treatment from '../../assets/images/treatment.png'
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const Ready = () => {
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse gap-80">
+                <div>
+                    <h1 className="text-5xl font-bold">Exceptional Dental
+                        <br />
+                        Care, on Your Terms</h1>
+                    <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <PrimaryButton>Get Started</PrimaryButton>
+                </div>
+                <div>
+                    <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Ready;
