f6d5bf5806b28a8a52dabd88cd8bf2849bc3f569
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-14 16:41:51 +0600

    new firebase added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyALFZ2UoKt1PJN7d67W6iFZqJK5LMlX7Uk",
+    authDomain: "doctors-portal-site-6dcd6.firebaseapp.com",
+    projectId: "doctors-portal-site-6dcd6",
+    storageBucket: "doctors-portal-site-6dcd6.appspot.com",
+    messagingSenderId: "152138709918",
+    appId: "1:152138709918:web:a8bd3bdf96959578c0748f",
 };
 
 // Initialize Firebase
